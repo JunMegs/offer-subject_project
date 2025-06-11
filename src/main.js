93e0480a86908d9d783519f175f9338b6f709585
Junward Megullas <meg.junward@gmail.com>
2025-06-11 10:00:33 +0800

    minor changes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'; // Ensure you have installed vuetify and its dependencies
 import router from './router/index';
 
 import 'vuetify/styles';
